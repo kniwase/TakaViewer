fecca5e8ffb7b38b097a94a28b7e8f6a364699c0
kniwase <kento.niwase@gmail.com>
2018-05-01 17:50:50 +0900

    画像表示用ウィンドウ

--- TakaViewer.py
+++ TakaViewer.py
@@ -2,6 +2,9 @@ import tkinter, os, cv2, numpy as np
 from tkinter import filedialog
 from niwaCV import niwaCV
 
+def do_nothing():
+    pass
+
 #フォルダ選択用関数
 def select_dir(event):
     def _select_dir():
@@ -25,24 +28,47 @@ def select_file(event):
 
 #メインウィンドウの定義
 #   メインウィンドウはファイル選択ボックスのあるウィンドウ
-main_win = tkinter.Tk()
-main_win.title('File List')
+class Main_wiondow(tkinter.Tk):
+    def __init__(self):
+        super(Main_wiondow, self).__init__()
+        self.title('File List')
 
 #フォルダ選択ボタンの定義
-button = tkinter.Button(main_win ,text='Slect Folder')
-button.bind('<Button-1>', select_dir)
-button.pack(side=tkinter.BOTTOM)
+class Sel_dir_button(tkinter.Button):
+    def __init__(self, main_win):
+        super(Sel_dir_button, self).__init__(main_win, text='Slect Folder')
+        self.bind('<Button-1>', select_dir)
+        self.pack(side=tkinter.BOTTOM)
 
 #ファイル選択ボックスの定義
-Listbox_scrollbar_y = tkinter.Scrollbar(main_win, orient=tkinter.VERTICAL)
-Listbox_scrollbar_x = tkinter.Scrollbar(main_win, orient=tkinter.HORIZONTAL)
-file_list_box = tkinter.Listbox(main_win, height=12, width=80, selectmode=tkinter.SINGLE, yscrollcommand=Listbox_scrollbar_y.set, xscrollcommand=Listbox_scrollbar_x.set)
-Listbox_scrollbar_y.config(command=file_list_box.yview)
-Listbox_scrollbar_y.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-Listbox_scrollbar_x.config(command=file_list_box.xview)
-Listbox_scrollbar_x.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-file_list_box.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
-file_list_box.bind('<<ListboxSelect>>', select_file)
+class File_list_box(tkinter.Listbox):
+    def __init__(self, main_win):
+        self.Listbox_scrollbar_y = tkinter.Scrollbar(main_win, orient=tkinter.VERTICAL)
+        self.Listbox_scrollbar_x = tkinter.Scrollbar(main_win, orient=tkinter.HORIZONTAL)
+        super(File_list_box, self).__init__(main_win, height=12, width=80, selectmode=tkinter.SINGLE, yscrollcommand=self.Listbox_scrollbar_y.set, xscrollcommand=self.Listbox_scrollbar_x.set)
+        self.Listbox_scrollbar_y.config(command=self.yview)
+        self.Listbox_scrollbar_y.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.Listbox_scrollbar_x.config(command=self.xview)
+        self.Listbox_scrollbar_x.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        self.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
+        self.bind('<<ListboxSelect>>', select_file)
+
+#画像表示用ウィンドウの定義
+class Image_win(tkinter.Toplevel):
+    def __init__(self, main_win):
+        super(Image_win, self).__init__(main_win)
+        self.title('Image')
+        self.protocol("WM_DELETE_WINDOW", do_nothing)
+
+main_win = Main_wiondow()
+sel_dir_button = Sel_dir_button(main_win)
+file_list_box = File_list_box(main_win)
+
+
+if 'image_win' in globals():
+    pass
+else:
+    image_win = Image_win(main_win)
 
 #メインループ
 main_win.mainloop()
